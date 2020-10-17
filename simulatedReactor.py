e76ffe68d465015acef8da434b8c9a5d6a666447
Jack Bodine <JackTaylorBodine@gmail.com>
2020-10-17 13:10:34 -0500

    Added setPressure() to simulatedReactor
    The setPressure function changes the pressure to a set figure. It operates slowly to simulate a real vacuum chamber. (But I really don't know what an appropriate speed would be.) It also lowers temperature as pressure gets lowered. (Again, I don't know the actual relationship between pressure and temp. so there is a placeholder function.)

--- simulatedReactor.py
+++ simulatedReactor.py
@@ -1,8 +1,38 @@
+import time
+
+def test(): 
+  myReactor = simulatedReactor
+  myReactor.setPressure(500000)
+
 class simulatedReactor: 
-  temperature = 0
+  temperature = 0.0
   pressure = 0
   current = 0
 
   def __init__(self):
-    self.pressure = 760,000   #This is the avg. pressure in microns at sea level.
-    self.temperature = 20     #Room temp in c.
+    self.pressure = 760000   #This is the avg. pressure at sea level.
+    self.temperature = 20.0     #Room temp in c.
+
+  def setPressure(self, n):
+    self.displayInfo()
+    if self.pressure == n:
+      return True
+    elif self.pressure > n:
+      self.pressure -= 1000
+      time.sleep(1)
+      self.setPressure(n)
+    elif self.pressure < n:
+      self.pressure += 1000
+      time.sleep(1)
+      self.setPressure(n)
+    else:
+      print("ERROR")
+
+  def updateTemp(self):
+    self.temperature = 21 - ((760000/self.pressure))
+
+  def displayInfo(self):
+    self.updateTemp()
+    print("Reactor Temperature: ", self.temperature)
+    print("Reactor Pressure: ", self.pressure)
+    print("Reactor Current: ", self.current)
