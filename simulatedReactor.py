921e5288bdca972c3f4c3f9103c9489ad2300c5e
Jack Bodine <JackTaylorBodine@gmail.com>
2020-10-23 17:46:17 -0500

    Added some new stuff after todays discussion.
    -Added new variables (Voltage, External Neutron Count)
    -Labeled each variable as "Read Only" or "Read/Write"
    -Added groundwork for random events.

--- simulatedReactor.py
+++ simulatedReactor.py
@@ -1,19 +1,25 @@
 import time
 import os
+import random
 
-def test(): 
-  myReactor = simulatedReactor
-  myReactor.setPressure(500000)
-
+#This class simulates a reactor. It used by the controller for testing purposes. 
+#This class never communicates with the user. All communications must be done through the controller.
+#Some "events" can happen randomly. It is up to the controller to both detect and respond to events.
 class simulatedReactor: 
-  temperature = 0.0
-  pressure = 0
-  current = 0
+  temperature = 0.0             #Read Only
+  pressure = 0                  #Read/Write
+  current = 0                   #Read Only
+  voltage = 0                   #Read/Write
+  externalNeutronCount = 0      #Read Only
+  deuterium = 0                 #Read/Write
+
+  doEvents = False              #Changing this will allow for random events
 
   def __init__(self):
     self.pressure = 760         #Normal pressure at sea level in torr?
     self.temperature = 20.0     #Room temp in c.
 
+  #This function sets the pressure to a given value.
   def setPressure(self, n):
     self.displayInfo()
     if self.pressure == n:
@@ -29,23 +35,58 @@ class simulatedReactor:
     else:
       print("ERROR")
 
-  def setCurrent(self, n):
+  #This function sets the voltage to a given value.
+  def setVoltage(self, n):
     self.displayInfo()
-    if self.current != n:
+    if self.voltage != n:
       time.sleep(1)
-      self.current = n
+      self.voltage = n
       self.displayInfo()
     else:
       print("ERROR")
 
-  def updateTemp(self):
+  def update(self):
+    if self.doEvents == True:
+      self.causeRandomEvent()
+
     #Totally random relationship between temperature and pressure.
     self.temperature = (self.pressure/760)*20 
+    #Another random relationship between voltage and current.
+    self.current = 3 * self.voltage - 0.05
+    #Another uncertian relationship between Neutrons and Voltage
+    self.externalNeutronCount = (self.voltage/5)*10
 
   def displayInfo(self):
     os.system('cls' if os.name == 'nt' else 'clear')
     #This is only for testing purposes. The controller will need to get and print this data. 
-    self.updateTemp()
-    print("Reactor Temperature: ", self.temperature)
-    print("Reactor Pressure: ", self.pressure)
-    print("Reactor Current: ", self.current)
+    self.update()
+    print("---Reactor Info---")
+
+    print("Temperature: ", self.temperature, "C")
+    print("Pressure: ", self.pressure, "Torr")
+    print("Current: ", self.current, "AMPS")
+    print("Voltage: ", self.voltage, "kV")
+    print("External Neutron Count: ", self.externalNeutronCount, "")
+
+  def causeRandomEvent(self):
+    events = ["vaccumLeak", "currentSurge", "powerEnd"]
+    rand = random.randrange(0,500,1)
+    if rand == 1:
+      randEventName = random.randrange(0,2,1)
+      self.event(events[randEventName])
+
+  #These will simulate potential events that could happen.
+  #It will be up to the controller to react and respond. 
+  def event(self, name):
+    if name == "vaccumLeak":
+      print("EVENT: Vaccum Leak")
+      rand = random.randrange(10,50,1)
+      self.pressure -= rand
+    elif name == "currentSurge":
+      print("EVENT: Surge in voltage")
+      rand = random.randrange(0,20,1)
+      self.voltage += rand
+    elif name == "powerEnd":
+      print("EVENT: Sudden Power Loss")
+      self.voltage = 0
+    self.displayInto()
